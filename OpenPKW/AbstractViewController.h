b7e4c3828764a3445ee35053caa3e3f93c6ca486
Piotr Tobolski <piotr.tobolski@me.com>
2015-04-24 20:17:03 +0200

    WelcomeViewController stub

--- OpenPKW/AbstractViewController.h
+++ OpenPKW/AbstractViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AbstractViewController.h
 //  OpenPKW
 //
 //  Created by Piotr Tobolski on 24.04.2015.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface AbstractViewController : UIViewController
 
 @end
-
---
+++ OpenPKW/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  OpenPKW
+//
+//  Created by Piotr Tobolski on 24.04.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "AbstractViewController.h"
+
+@interface WelcomeViewController : AbstractViewController
+
+@end
