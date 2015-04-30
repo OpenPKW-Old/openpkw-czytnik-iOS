efadd47b80ce5a4f72280348aa3a1fc4dfcb98bf
sloik <stocki.lukasz@gmail.com>
2015-04-30 22:53:07 +0200

    Adds ElectoralCommissionSelectionViewController to the project.

---
+++ OpenPKW/ViewControllers/ElectoralCommissionSelection/ElectoralCommissionSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ElectoralCommissionSelectionViewController.h
+//  OpenPKW
+//
+//  Created by Developer iOS on 30.04.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "AbstractViewController.h"
+
+@interface ElectoralCommissionSelectionViewController : AbstractViewController
+
+@end
---
+++ OpenPKW/ViewControllers/ElectoralCommissionSelection/ElectoralCommissionSelectionViewController.m
@@ -0,0 +1,17 @@
+//
+//  ElectoralCommissionSelectionViewController.m
+//  OpenPKW
+//
+//  Created by Developer iOS on 30.04.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "ElectoralCommissionSelectionViewController.h"
+
+@interface ElectoralCommissionSelectionViewController ()
+
+@end
+
+@implementation ElectoralCommissionSelectionViewController
+
+@end
