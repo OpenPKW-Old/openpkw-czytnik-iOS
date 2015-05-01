94e83372224ecac4e34514e1312760c56a1df8d8
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-05-01 19:28:52 +0200

    Fixes broken constraints when view is resized.

--- OpenPKW/CustomUI/VerticalTitleInputFieldCell.m
+++ OpenPKW/CustomUI/VerticalTitleInputFieldCell.m
@@ -10,6 +10,15 @@
 
 @implementation VerticalTitleInputFieldCell
 
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    
+    // why: http://stackoverflow.com/questions/19132908/auto-layout-constraints-issue-on-ios7-in-uitableviewcell
+    self.contentView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    
+    return self;
+}
+
 - (void)configureCellWithRowDescriptor:(VerticalTitleInputFieldRowDescriptor *)descriptor {
     
     self.titleLabel.text = descriptor.title;
