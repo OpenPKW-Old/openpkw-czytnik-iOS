44bb60b41faf0d1be391b5da3381560849fbc14d
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-05-01 14:31:58 +0200

    Adds new cell subclass.

---
+++ OpenPKW/CustomUI/VerticalTitleInputFieldCell.h
@@ -0,0 +1,13 @@
+//
+//  VerticalTitleInputFieldCell.h
+//  OpenPKW
+//
+//  Created by Lukasz Stocki on 01.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VerticalTitleInputFieldCell : UITableViewCell
+
+@end
---
+++ OpenPKW/CustomUI/VerticalTitleInputFieldCell.m
@@ -0,0 +1,13 @@
+//
+//  VerticalTitleInputFieldCell.m
+//  OpenPKW
+//
+//  Created by Lukasz Stocki on 01.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "VerticalTitleInputFieldCell.h"
+
+@implementation VerticalTitleInputFieldCell
+
+@end
