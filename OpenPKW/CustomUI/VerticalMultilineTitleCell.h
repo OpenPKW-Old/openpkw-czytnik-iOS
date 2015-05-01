923f9425c5cebf914c6e610bc306c3f48d99a568
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-05-01 19:50:24 +0200

    Adds multiline title cell to the project.

---
+++ OpenPKW/CustomUI/VerticalMultilineTitleCell.h
@@ -0,0 +1,13 @@
+//
+//  VerticalMultilineTitleCell.h
+//  OpenPKW
+//
+//  Created by Lukasz Stocki on 01.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "BaseCell.h"
+
+@interface VerticalMultilineTitleCell : BaseCell
+
+@end
---
+++ OpenPKW/CustomUI/VerticalMultilineTitleCell.m
@@ -0,0 +1,13 @@
+//
+//  VerticalMultilineTitleCell.m
+//  OpenPKW
+//
+//  Created by Lukasz Stocki on 01.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "VerticalMultilineTitleCell.h"
+
+@implementation VerticalMultilineTitleCell
+
+@end
