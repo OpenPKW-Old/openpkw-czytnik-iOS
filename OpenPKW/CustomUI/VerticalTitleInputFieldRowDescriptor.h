7e167613c4282a2dea76e8f1697c3c1240cc35ee
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-05-01 21:23:56 +0200

    Adds keyboard type to the row descriptor.

--- OpenPKW/CustomUI/VerticalTitleInputFieldRowDescriptor.h
+++ OpenPKW/CustomUI/VerticalTitleInputFieldRowDescriptor.h
@@ -6,14 +6,19 @@
 //  Copyright (c) 2015 OpenPKW. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import UIKit;
 
 @interface VerticalTitleInputFieldRowDescriptor : NSObject
 
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, copy) NSString *inputPlaceholder;
+@property (nonatomic, assign) UIKeyboardType keyboardType;
 
 + (instancetype)rowDescriptorWithTitle:(NSString *)title
                       inputPlaceholder:(NSString *)inputPlaceholder;
+
++ (instancetype)rowDescriptorWithTitle:(NSString *)title
+                      inputPlaceholder:(NSString *)inputPlaceholder
+                          keyboardType:(UIKeyboardType)keyboardType;
 
 @end
--- OpenPKW/CustomUI/VerticalTitleInputFieldRowDescriptor.m
+++ OpenPKW/CustomUI/VerticalTitleInputFieldRowDescriptor.m
@@ -13,10 +13,20 @@
 + (instancetype)rowDescriptorWithTitle:(NSString *)title
                       inputPlaceholder:(NSString *)inputPlaceholder {
     
+    return [self rowDescriptorWithTitle:title
+                       inputPlaceholder:inputPlaceholder
+                           keyboardType:UIKeyboardTypeDefault];
+}
+
++ (instancetype)rowDescriptorWithTitle:(NSString *)title
+                      inputPlaceholder:(NSString *)inputPlaceholder
+                          keyboardType:(UIKeyboardType)keyboardType {
+    
     VerticalTitleInputFieldRowDescriptor *descriptor = [[self alloc] init];
     
     descriptor.title = title;
     descriptor.inputPlaceholder = inputPlaceholder;
+    descriptor.keyboardType = keyboardType;
     
     return descriptor;
 }
