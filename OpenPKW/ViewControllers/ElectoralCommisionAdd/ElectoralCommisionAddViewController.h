19fcaff5261d204506028105ad2bfb21c4714911
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-05-01 12:10:22 +0200

    Adds Electoral Commission Add View Controller to the project and hooks up it in the Storyboard.

---
+++ OpenPKW/ViewControllers/ElectoralCommisionAdd/ElectoralCommisionAddViewController.h
@@ -0,0 +1,13 @@
+//
+//  ElectoralCommisionAddViewController.h
+//  OpenPKW
+//
+//  Created by Lukasz Stocki on 01.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "AbstractViewController.h"
+
+@interface ElectoralCommisionAddViewController : AbstractViewController
+
+@end
---
+++ OpenPKW/ViewControllers/ElectoralCommisionAdd/ElectoralCommisionAddViewController.m
@@ -0,0 +1,13 @@
+//
+//  ElectoralCommisionAddViewController.m
+//  OpenPKW
+//
+//  Created by Lukasz Stocki on 01.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "ElectoralCommisionAddViewController.h"
+
+@implementation ElectoralCommisionAddViewController
+
+@end
