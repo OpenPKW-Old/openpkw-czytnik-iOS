bd932427e2ccd4b95d1e9a58aaccaedb44997922
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-05-10 13:02:19 +0200

    Adds data source for the ProtocolFilling View Controller.

---
+++ OpenPKW/ViewControllers/ProtocolFilling/ProtocolFillingDataSource.h
@@ -0,0 +1,17 @@
+//
+//  ProtocolFillingDataSource.h
+//  OpenPKW
+//
+//  Created by Lukasz Stocki on 10.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+@import UIKit;
+
+#import "TableViewDescriptor.h"
+
+@interface ProtocolFillingDataSource : NSObject <UITableViewDataSource>
+
+- (instancetype)initWithTableDescriptor:(TableViewDescriptor *)tableDescriptor;
+
+@end
---
+++ OpenPKW/ViewControllers/ProtocolFilling/ProtocolFillingDataSource.m
@@ -0,0 +1,47 @@
+//
+//  ProtocolFillingDataSource.m
+//  OpenPKW
+//
+//  Created by Lukasz Stocki on 10.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "ProtocolFillingDataSource.h"
+
+@interface ProtocolFillingDataSource ()
+
+@property (nonatomic, strong) TableViewDescriptor *tableDescriptor;
+
+@end
+
+@implementation ProtocolFillingDataSource
+
+#pragma mark - LifeCycle
+
+- (instancetype)initWithTableDescriptor:(TableViewDescriptor *)tableDescriptor {
+    
+    self = [super init];
+    if (self == nil) {
+        return nil;
+    }
+    
+    return self;
+}
+
+#pragma mark - Setup
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 0;
+}
+
+@end
