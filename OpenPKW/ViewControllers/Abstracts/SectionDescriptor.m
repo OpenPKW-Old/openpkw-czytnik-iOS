6aff292712bc3599f6c8211fd49a56ecf5b5ff48
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-05-10 13:00:26 +0200

    Adds table view section descriptors to the project.

---
+++ OpenPKW/ViewControllers/Abstracts/SectionDescriptor.m
@@ -0,0 +1,50 @@
+//
+//  SectionDescriptor.m
+//  OpenPKW
+//
+//  Created by Lukasz Stocki on 10.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "SectionDescriptor.h"
+
+@interface SectionDescriptor ()
+
+@property (nonatomic, strong) NSMutableArray *rows;
+
+@end
+
+@implementation SectionDescriptor
+
++ (instancetype)sectionDescriptorWithTitle:(NSString *)title {
+    return [[self alloc] initWithTitle:title];
+}
+
+- (instancetype)initWithTitle:(NSString *)title {
+    self = [super init];
+    
+    if (self == nil) {
+        return nil;
+    }
+    
+    _title = [title copy];
+    _rows = [NSMutableArray array];
+    
+    return self;
+}
+
+#pragma mark - Public API
+
+- (void)addRowDescriptor:(RowDescriptor *)rowDescriptor {
+    NSAssert([rowDescriptor isKindOfClass:[RowDescriptor class]], @"Item should be of a RowDescriptor class");
+
+    [self.rows addObject:rowDescriptor];
+}
+
+- (void)addRowsDescriptors:(NSArray *)rowDescriptors {
+    for (id rowDescriptor in rowDescriptors) {
+        [self addRowDescriptor:rowDescriptor];
+    }
+}
+
+@end
