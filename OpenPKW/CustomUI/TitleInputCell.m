cd57ff0b4c3f1883bf0ed0a699f885a99a516547
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-05-16 17:25:46 +0200

    Adds title input cell.

---
+++ OpenPKW/CustomUI/TitleInputCell.m
@@ -0,0 +1,19 @@
+//
+//  TitleInputCell.m
+//  OpenPKW
+//
+//  Created by Lukasz Stocki on 16.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "TitleInputCell.h"
+
+@implementation TitleInputCell
+
+- (void)configureCellWithRowDescriptor:(RowDescriptor *)descriptor {
+
+    self.label.text = descriptor.displayText;
+    self.inputTextField.placeholder = descriptor.secondaryText;
+}
+
+@end
