5551eb2a6cf2a6f006725a6ff4d76a7ce0774b30
sloik <stocki.lukasz@gmail.com>
2015-05-19 21:54:48 +0200

    Adds image cell to the project.

---
+++ OpenPKW/CustomUI/ImageCell.h
@@ -0,0 +1,14 @@
+//
+//  ImageCell.h
+//  OpenPKW
+//
+//  Created by Developer iOS on 19.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "BaseCell.h"
+
+@interface ImageCell : BaseCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+@end
