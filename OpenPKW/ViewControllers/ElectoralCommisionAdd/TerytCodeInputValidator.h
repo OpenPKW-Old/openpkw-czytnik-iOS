af7baeadb502b7e3c8a2e0d7e400914e806a2ac1
sloik <stocki.lukasz@gmail.com>
2015-05-21 21:45:41 +0200

    Adds initial implementation for the terry code validator.

---
+++ OpenPKW/ViewControllers/ElectoralCommisionAdd/TerytCodeInputValidator.h
@@ -0,0 +1,15 @@
+//
+//  TerytCodeInputValidator.h
+//  OpenPKW
+//
+//  Created by Developer iOS on 21.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TerytCodeInputValidator : NSObject
+
++ (BOOL)isValidTerrytCode:(NSString *)terytCode;
+
+@end
---
+++ OpenPKW/ViewControllers/ElectoralCommisionAdd/TerytCodeInputValidator.m
@@ -0,0 +1,17 @@
+//
+//  TerytCodeInputValidator.m
+//  OpenPKW
+//
+//  Created by Developer iOS on 21.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "TerytCodeInputValidator.h"
+
+@implementation TerytCodeInputValidator
+
++ (BOOL)isValidTerrytCode:(NSString *)terytCode {
+	return YES;
+}
+
+@end
