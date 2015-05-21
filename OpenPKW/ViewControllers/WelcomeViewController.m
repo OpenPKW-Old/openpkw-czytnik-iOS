d9a691e487d4aee6049d098835e4acfd6e572099
sloik <stocki.lukasz@gmail.com>
2015-05-21 23:50:45 +0200

    Customises welcome view controller and correctly loads the other vcs according to the flow.

--- OpenPKW/ViewControllers/WelcomeViewController.m
+++ OpenPKW/ViewControllers/WelcomeViewController.m
@@ -8,11 +8,28 @@
 
 #import "WelcomeViewController.h"
 
+#import "UIStoryboard+Storyboards.h"
+
+static NSString *const kWebViewSegueId = @"ShowWebView";
+
+@interface WelcomeViewController ()
+
+@property (weak, nonatomic) IBOutlet UIControl *goToLogin;
+@property (weak, nonatomic) IBOutlet UIControl *goToPage;
+
+@end
+
+
 @implementation WelcomeViewController
 
 - (void)viewWillAppear:(BOOL)animated {
-	[self.navigationController setNavigationBarHidden:YES
-											 animated:animated];
+	
+	if (self.navigationController.navigationBarHidden == NO) {
+		
+		[self.navigationController setNavigationBarHidden:YES
+												 animated:animated];
+	}
+
 	
 	[super viewWillAppear:animated];
 }
@@ -22,6 +39,35 @@
 											 animated:animated];
 	
 	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidLoad {
+	[self.navigationController setNavigationBarHidden:YES
+											 animated:NO];
+	
+	self.goToLogin.backgroundColor = [UIColor clearColor];
+	self.goToPage.backgroundColor = [UIColor clearColor];
+}
+
+- (void)presentBeforeLogin {
+	UIWindow *appDelegateWindow = [[[UIApplication sharedApplication] delegate] window];
+	UIViewController *vcToPresent = [[UIStoryboard beforeLoginStoryboard] instantiateInitialViewController];
+	
+	[UIView transitionWithView:appDelegateWindow
+					  duration:0.5
+					   options:UIViewAnimationOptionTransitionFlipFromLeft
+					animations:^{
+						appDelegateWindow.rootViewController = vcToPresent;
+					} completion:nil];
+}
+
+- (IBAction)goToWebView:(id)sender {
+	[self performSegueWithIdentifier:kWebViewSegueId
+							  sender:nil];
+}
+
+- (IBAction)goToLoginScreen:(id)sender {
+	[self presentBeforeLogin];
 }
 
 @end
