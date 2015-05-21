296d0b6e15b7c8c6c36bb7481f0a5abc28023a3c
sloik <stocki.lukasz@gmail.com>
2015-05-21 23:49:43 +0200

    Adds RWD vc controller to the project.

---
+++ OpenPKW/ViewControllers/RWDViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWDViewController.h
+//  OpenPKW
+//
+//  Created by Developer iOS on 21.05.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "AbstractViewController.h"
+
+@interface RWDViewController : AbstractViewController
+
+@end
