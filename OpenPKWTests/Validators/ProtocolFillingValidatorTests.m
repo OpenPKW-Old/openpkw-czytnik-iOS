1f502e20daf452a2bbcf09ef9376140cc6d2c816
Lukasz Stocki <lukasz.stocki@allegrogroup.com>
2015-05-24 16:50:09 +0200

    Updates class after scope change and adds unit test for confimarion that everything works.

--- OpenPKWTests/Validators/ProtocolFillingValidatorTests.m
+++ OpenPKWTests/Validators/ProtocolFillingValidatorTests.m
@@ -29,6 +29,22 @@ ProtocolForCommissionRequestDto *(^correctProtocolDTO)() = ^{
     return dto;
 };
 
+ProtocolForCommissionRequestDto * (^invalidProtocolDTO)() = ^{
+    ProtocolForCommissionRequestDto *dto = [[ProtocolForCommissionRequestDto alloc] init];
+    
+    dto.permittedPeopleCount = 100;  // how many people can vote
+    dto.votingPeopleCount    = 150;  // how many people did vote
+    
+    dto.cardsValidCount   = 40; // valid cards
+    dto.votesValidCount   = 30; // valid votes
+    dto.votesInvalidCount = 10; // invalid votes
+    
+    dto.votesForCandidate = @{ @"1": @20,
+                               @"2": @10 };
+    
+    return dto;
+};
+
 describe(@"ProtocolFillingValidatorTests", ^{
     
     __block ProtocolFillingValidator *systemUnderTest = nil;
@@ -41,32 +57,109 @@ describe(@"ProtocolFillingValidatorTests", ^{
         context(@"missing", ^{
             it(@"should return a validation result containg missing protocol DTO", ^{
                 
-                ProtocolFillingValidatorResult *result = [systemUnderTest validateProtocol:nil];
+                ProtocolFillingResult result = [systemUnderTest validateProtocol:nil];
                 
-                BOOL hasMissingDTOFSet = OptionsHasValue(result.hardValidation, ProtocolFillingResultInvalidMissingProtocolDTO);
+                BOOL hasMissingDTOFSet = OptionsHasValue(result, ProtocolFillingResultInvalidMissingProtocolDTO);
                 
                 [[theValue(hasMissingDTOFSet) should] beYes];
             });
         });
         
         context(@"present", ^{
-            it(@"should return a validation result containg missing protocol DTO", ^{
+            it(@"should return a validation result not containg missing protocol DTO", ^{
                 
-                ProtocolFillingValidatorResult *result = [systemUnderTest validateProtocol:[[ProtocolForCommissionRequestDto alloc] init]];
+                ProtocolFillingResult result = [systemUnderTest validateProtocol:[[ProtocolForCommissionRequestDto alloc] init]];
                 
-                BOOL hasMissingDTOFSet = OptionsHasValue(result.hardValidation, ProtocolFillingResultInvalidMissingProtocolDTO);
+                BOOL hasMissingDTOFSet = OptionsHasValue(result, ProtocolFillingResultInvalidMissingProtocolDTO);
                 
                 [[theValue(hasMissingDTOFSet) should] beNo];
             });
         });
     });
     
+    context(@"voting people count", ^{
+        context(@"is valid then", ^{
+            it(@"should not contain invalid voting people count", ^{
+                ProtocolFillingResult result = [systemUnderTest validateProtocol:correctProtocolDTO()];
+                
+                BOOL hasInvalidVotingPopleCount = OptionsHasValue(result, ProtocolFillingResultInvalidVotingPeopleCount);
+                
+                [[theValue(hasInvalidVotingPopleCount) should] beNo];
+            });
+        });
+        
+        context(@"is invalid then", ^{
+            it(@"should contain invalid voting people count", ^{
+                ProtocolFillingResult result = [systemUnderTest validateProtocol:invalidProtocolDTO()];
+                
+                BOOL hasInvalidVotingPopleCount = OptionsHasValue(result, ProtocolFillingResultInvalidVotingPeopleCount);
+                
+                [[theValue(hasInvalidVotingPopleCount) should] beYes];
+            });
+        });
+    });
+    
+    context(@"cards valid count", ^{
+        context(@"all fields are valid", ^{
+            it(@"shoud not contain invalid cards count", ^{
+                ProtocolFillingResult result = [systemUnderTest validateProtocol:correctProtocolDTO()];
+                
+                BOOL hasInvalidVotingPopleCount = OptionsHasValue(result, ProtocolFillingResultInvalidCardsValidCount);
+                
+                [[theValue(hasInvalidVotingPopleCount) should] beNo];
+            });
+        });
+        
+        context(@"votes valid count + votes invalid count is different than cards valid count", ^{
+            it(@"shoud contain invalid cards count", ^{
+                
+                ProtocolForCommissionRequestDto *dataToTest = correctProtocolDTO();
+                dataToTest.cardsValidCount = 100;
+                dataToTest.votesValidCount = 200;
+                dataToTest.votesInvalidCount = 100;
+                
+                ProtocolFillingResult result = [systemUnderTest validateProtocol:dataToTest];
+                
+                BOOL hasInvalidVotingPopleCount = OptionsHasValue(result, ProtocolFillingResultInvalidCardsValidCount);
+                
+                [[theValue(hasInvalidVotingPopleCount) should] beYes];
+            });
+        });
+        
+        context(@"votes invalid count + all candidate votes is differant than cards valid count", ^{
+            ProtocolForCommissionRequestDto *dataToTest = correctProtocolDTO();
+            dataToTest.cardsValidCount = 10;
+            dataToTest.votesInvalidCount = 20;
+            dataToTest.votesForCandidate = @{@"1": @50};
+            
+            ProtocolFillingResult result = [systemUnderTest validateProtocol:dataToTest];
+            
+            BOOL hasInvalidVotingPopleCount = OptionsHasValue(result, ProtocolFillingResultInvalidCardsValidCount);
+            
+            [[theValue(hasInvalidVotingPopleCount) should] beYes];
+        });
+        
+        context(@"all validation rules are broaken", ^{
+            ProtocolForCommissionRequestDto *dataToTest = correctProtocolDTO();
+            dataToTest.cardsValidCount = 100;
+            dataToTest.votesValidCount = 200;
+            dataToTest.votesInvalidCount = 20;
+            dataToTest.votesForCandidate = @{@"1": @550};
+            
+            ProtocolFillingResult result = [systemUnderTest validateProtocol:dataToTest];
+            
+            BOOL hasInvalidVotingPopleCount = OptionsHasValue(result, ProtocolFillingResultInvalidCardsValidCount);
+            
+            [[theValue(hasInvalidVotingPopleCount) should] beYes];
+        });
+    });
+    
     context(@"correct protocol dto", ^{
         it(@"should return a valid validation result", ^{
             
-            ProtocolFillingValidatorResult *result = [systemUnderTest validateProtocol:correctProtocolDTO()];
+            ProtocolFillingResult result = [systemUnderTest validateProtocol:correctProtocolDTO()];
 
-            BOOL isValid = (result.hardValidation == ProtocolFillingResultValid);
+            BOOL isValid = (result == ProtocolFillingResultValid);
             
             [[theValue(isValid) should] beYes];
         });
