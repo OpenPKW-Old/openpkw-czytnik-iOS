19c130765f7869b3ca847663d2be86f1d4b1f456
sloik <stocki.lukasz@gmail.com>
2015-07-14 21:11:59 +0200

    Adds a stub class for QRCodeScanner VC.

---
+++ OpenPKW/ViewControllers/QRCodeScanner/QRCodeScannerViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRCodeScannerViewController.h
+//  OpenPKW
+//
+//  Created by Developer iOS on 14.07.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "AbstractViewController.h"
+
+@interface QRCodeScannerViewController : AbstractViewController
+
+@end
---
+++ OpenPKW/ViewControllers/QRCodeScanner/QRCodeScannerViewController.m
@@ -0,0 +1,13 @@
+//
+//  QRCodeScannerViewController.m
+//  OpenPKW
+//
+//  Created by Developer iOS on 14.07.2015.
+//  Copyright (c) 2015 OpenPKW. All rights reserved.
+//
+
+#import "QRCodeScannerViewController.h"
+
+@implementation QRCodeScannerViewController
+
+@end
