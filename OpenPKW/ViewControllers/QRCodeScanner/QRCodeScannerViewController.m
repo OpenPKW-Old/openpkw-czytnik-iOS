40c4d383468edac03b49a24f1837a379573b04b1
sloik <stocki.lukasz@gmail.com>
2015-07-15 19:40:22 +0200

    Updates flow to get the scanner working.

--- OpenPKW/ViewControllers/QRCodeScanner/QRCodeScannerViewController.m
+++ OpenPKW/ViewControllers/QRCodeScanner/QRCodeScannerViewController.m
@@ -8,6 +8,55 @@
 
 #import "QRCodeScannerViewController.h"
 
+#import "MTBBarcodeScanner.h"
+
+@interface QRCodeScannerViewController ()
+
+@property (nonatomic, strong, nullable) MTBBarcodeScanner *scanner;
+@property (weak, nonatomic) IBOutlet UIView *scannerView;
+
+@end
+
 @implementation QRCodeScannerViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	
+	[self configureNaviagationItems];
+	
+	self.scanner = [[MTBBarcodeScanner alloc] initWithMetadataObjectTypes:@[AVMetadataObjectTypeQRCode]
+															  previewView:self.scannerView];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+	[super viewDidAppear:animated];
+	
+	[self.scanner startScanningWithResultBlock:^(NSArray *codes) {
+		for (AVMetadataMachineReadableCodeObject *code in codes) {
+			NSLog(@"Found code: %@", code.stringValue);
+		}
+	}];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+	[super viewDidDisappear:animated];
+	
+	[self.scanner stopScanning];
+}
+
+#pragma mark - Setup
+
+- (void)configureNaviagationItems {
+	UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+																				target:self
+																				action:@selector(doneButtonPressed)];
+	
+	self.navigationItem.rightBarButtonItem = doneButton;
+}
+
+#pragma mark - User Interaction
+- (void)doneButtonPressed {
+	[self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
